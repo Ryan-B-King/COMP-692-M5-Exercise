e9e6348e5ee71f943bb20552db776b4d91eb3efd
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-06-06 12:48:27 -0700

    setup

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import {} from 'dotenv/config'
+import routes from './routes/routes.js'
+
+const app = express()
+
+app.use('/api/v1/employees', routes)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
